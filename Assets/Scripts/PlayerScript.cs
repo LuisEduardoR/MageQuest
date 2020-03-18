71f3697e70ef330211f5731b5055b779b34b896e
Luís Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2020-03-18 12:22:07 -0300

    Started remaking scripts
    - Added UI back
    
    - Improved scene lighting

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+namespace MageQuest.Player
+{
+
+    [RequireComponent(typeof(Movimentacao))]
+    public class PlayerScript : MonoBehaviour
+    {
+
+        private int health;
+        public int Health {
+            get  
+            {
+                return health;
+            }
+            set
+            {
+                health = Mathf.Clamp(value, 0, 100);
+                UpdateUI();
+            }
+        }
+
+        private int mana;
+        public int Mana {
+            get
+            {
+                return mana;
+            }
+            set
+            {
+                mana = Mathf.Clamp(value, 0, 100);
+                UpdateUI();
+            }
+        }
+
+        [System.Serializable]
+        public class UI
+        {
+            public TMP_Text healthText;
+            public TMP_Text manaText;
+        }
+        [SerializeField] UI playerUi;
+
+
+        public void Start()
+        {
+            Health = 100;
+            Mana = 100;
+        }
+
+        public void Damage(int amount)
+        {
+            Health -= amount;
+            if(Health == 0)
+            {
+                Die();
+            }
+
+        }
+
+        private void Die()
+        {
+            SceneManager.LoadScene("Morte");
+        }
+
+        public bool GiveHealth(int amount)
+        {
+            
+            if(Health == 100) 
+            {
+                return false;
+            }  
+
+            Health += amount;
+            return true;
+
+        }
+
+        public bool GiveMana(int amount)
+        {
+
+            if(Mana == 100) 
+            {
+                return false;
+            }
+
+            Mana += amount;
+            return true;
+
+        }
+
+        void UpdateUI() {
+
+            playerUi.healthText.text = Health.ToString();
+            playerUi.manaText.text = Mana.ToString();
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Potion.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MageQuest.Player;
+
+namespace MageQuest.Potions
+{
+        
+    public class Potion : MonoBehaviour
+    {
+
+        public enum Type { Health, Mana }
+
+        public Type potionType = Type.Health;
+        public int amount = 25;
+
+        void OnTriggerEnter(Collider other)
+        {
+            
+            if(other.tag == "Player")
+            {
+
+                bool collected = false;
+                PlayerScript player = other.GetComponent<PlayerScript>();
+
+                if(potionType == Type.Health) 
+                {
+                    collected = player.GiveHealth(amount);
+                }
+
+                if(potionType == Type.Mana) 
+                {
+                    collected = player.GiveMana(amount);
+                }
+
+                if(collected)
+                {
+                    Destroy(gameObject);
+                }
+
+            }
+
+        }
+    }
+
+}
